bb1eee3f03107081f2ca463843872aa5a5fed8ec
Julio Sene França <julio.sene@gmail.com>
2017-03-07 22:51:52 -0800

    Update configure.rb
    apt_repository 'nginx-php' do
      key          'http://nginx.org/keys/nginx_signing.key'
      uri          'ppa:nginx/stable'
      distribution node['lsb']['codename']
    end
    apt_update 'all platforms' do
      action :update
    end
    apt_package 'nginx' do
      action :install
      options '-yf'
    end
    service 'nginx' do
      supports status: true, restart: true, reload: true
      action :enable
    end

--- configure.rb
+++ configure.rb
@@ -1,16 +1,3 @@
-apt_repository 'nginx-php' do
-  key          'http://nginx.org/keys/nginx_signing.key'
-  uri          'ppa:nginx/stable'
-  distribution node['lsb']['codename']
-end
 apt_update 'all platforms' do
   action :update
-end
-apt_package 'nginx' do
-  action :install
-  options '-yf'
-end
-service 'nginx' do
-  supports status: true, restart: true, reload: true
-  action :enable
 end
