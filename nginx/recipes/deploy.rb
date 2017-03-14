ee4ddad427e6f14223abff19d1481ac2beec8bdf
Julio Sene França <julio.sene@gmail.com>
2017-03-13 20:35:11 -0700

    Update deploy.rb

--- nginx/recipes/deploy.rb
+++ nginx/recipes/deploy.rb
@@ -1,5 +1,13 @@
+app = search("aws_opsworks_app").first
+Chef::Log.info("********** The app's short name is '#{app['shortname']}' **********")
+Chef::Log.info("********** The app's URL is '#{app['app_source']['url']}' **********")
+
+search("aws_opsworks_app").each do |app|
+  Chef::Log.info("********** The app's short name is '#{app['shortname']}' **********")
+  Chef::Log.info("********** The app's URL is '#{app['app_source']['url']}' **********")
+end
 deploy 'wordpress' do
-  path deploy[:deploy_to]
+  path node[:deploy][:wordpress][:deploy_to]
   user 'root'
   group 'root'
   action :deploy
