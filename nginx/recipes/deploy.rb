ec89e45a4da3d26f6eb6973bfe6709be57aadc42
Julio Sene França <julio.sene@gmail.com>
2017-03-13 20:40:02 -0700

    Update deploy.rb

--- nginx/recipes/deploy.rb
+++ nginx/recipes/deploy.rb
@@ -1,5 +1,5 @@
 app = search("aws_opsworks_app").first
-Chef::Log.info("********** The app's short name is '#{app['shortname']}' **********")
+Chef::Log.info("********** #node[:deploy][:wordpress][:deploy_to] *** The app's short name is '#{app['shortname']}' **********")
 Chef::Log.info("********** The app's URL is '#{app['app_source']['url']}' **********")
 
 search("aws_opsworks_app").each do |app|
@@ -7,7 +7,7 @@ search("aws_opsworks_app").each do |app|
   Chef::Log.info("********** The app's URL is '#{app['app_source']['url']}' **********")
 end
 deploy 'wordpress' do
-  path node[:deploy][:wordpress][:deploy_to]
+  path 'https://github.com/WordPress/WordPress.git'
   user 'root'
   group 'root'
   action :deploy
